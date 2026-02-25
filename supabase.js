5dfeaae6a9f3cb704c4c1f1027d5c61268a440b6
mailobscurateam <mail.obscurateam@gmail.com>
2026-02-25 11:40:35 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
  const CONFIG = {
-            SUPABASE_URL: 'https://pkqpvnrtxtqlngwtfnhy.supabase.co',
-            SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrcXB2bnJ0eHRxbG5nd3Rmbmh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzNzQxNTUsImV4cCI6MjA4NTk1MDE1NX0.wrYYlIzt1slOXkbUAqAaSI1bA9xJ2N8QhNHSRJIkZR0'
+            SUPABASE_URL: 'https://acxndpvztflskdnkxekn.supabase.co',
+            SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjeG5kcHZ6dGZsc2tkbmt4ZWtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5OTk1MDUsImV4cCI6MjA4NzU3NTUwNX0.VXg94z121tZGbN4TAm0MwclBaqf1MVWp5cJiIncosv0'
         };
